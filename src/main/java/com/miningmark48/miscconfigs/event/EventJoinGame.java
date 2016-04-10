46475d83943db75d1eb6ee674451510ab91f3d86
MiningMark48 <miningmark48@gmail.com>
2016-04-10 14:29:44 -0400

    Ability to set cutom chat message, with colors

---
+++ src/main/java/com/miningmark48/miscconfigs/event/EventJoinGame.java
@@ -0,0 +1,30 @@
+package com.miningmark48.miscconfigs.event;
+
+import com.miningmark48.miscconfigs.handler.ConfigurationHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.EnumChatFormatting;
+
+public class EventJoinGame{
+
+        @SubscribeEvent
+        public void onJoin(PlayerEvent.PlayerLoggedInEvent e){
+            if (!e.player.getEntityWorld().isRemote) {
+                if (ConfigurationHandler.doChatMessage) {
+                    if (ConfigurationHandler.chatMessageColor == 1) {
+                        e.player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.RED + ConfigurationHandler.chatMessage));
+                    }else if (ConfigurationHandler.chatMessageColor == 2) {
+                        e.player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.GREEN + ConfigurationHandler.chatMessage));
+                    }else if (ConfigurationHandler.chatMessageColor == 3) {
+                        e.player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.BLUE + ConfigurationHandler.chatMessage));
+                    }else if (ConfigurationHandler.chatMessageColor == 4) {
+                        e.player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.GOLD + ConfigurationHandler.chatMessage));
+                    }else{
+                        e.player.addChatComponentMessage(new ChatComponentTranslation(ConfigurationHandler.chatMessage));
+                    }
+                }
+            }
+        }
+
+}
