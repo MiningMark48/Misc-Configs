c11935ddc026afe7f0e2598de295e3dd1dd1ca94
MiningMark48 <miningmark48@gmail.com>
2016-04-10 20:53:21 -0400

    Added the ability to have the chat message be sent only once

--- src/main/java/com/miningmark48/miscconfigs/event/EventJoinGame.java
+++ src/main/java/com/miningmark48/miscconfigs/event/EventJoinGame.java
@@ -1,34 +1,57 @@
 package com.miningmark48.miscconfigs.event;
 
 import com.miningmark48.miscconfigs.handler.ConfigurationHandler;
+import com.miningmark48.miscconfigs.reference.Reference;
+import com.miningmark48.miscconfigs.utility.LogHelper;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.EnumChatFormatting;
 
+import java.sql.Ref;
 import java.util.StringTokenizer;
 
 public class EventJoinGame{
 
-        @SubscribeEvent
-        public void onJoin(PlayerEvent.PlayerLoggedInEvent e){
+    @SubscribeEvent
+    public void onJoin(PlayerEvent.PlayerLoggedInEvent e){
 
-            if (!e.player.getEntityWorld().isRemote) {
+        if (!e.player.getEntityWorld().isRemote) {
 
-                if (ConfigurationHandler.doChatMessage) {
-                    if (ConfigurationHandler.chatMessageColor == 1) {
-                        e.player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.RED + ConfigurationHandler.chatMessage));
-                    }else if (ConfigurationHandler.chatMessageColor == 2) {
-                        e.player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.GREEN + ConfigurationHandler.chatMessage));
-                    }else if (ConfigurationHandler.chatMessageColor == 3) {
-                        e.player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.BLUE + ConfigurationHandler.chatMessage));
-                    }else if (ConfigurationHandler.chatMessageColor == 4) {
-                        e.player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.GOLD + ConfigurationHandler.chatMessage));
-                    }else{
-                        e.player.addChatComponentMessage(new ChatComponentTranslation(ConfigurationHandler.chatMessage));
-                    }
+            NBTTagCompound tag = e.player.getEntityData();
+
+            if(tag.getString(Reference.MOD_ID + "doChatMessage") == "") {
+                tag.setString(Reference.MOD_ID + "doChatMessage", "send");
+            }
+
+            if (ConfigurationHandler.sendChatMessageOnce) {
+                if (tag.getString(Reference.MOD_ID + "doChatMessage") == "send") {
+                    sendChatMessage(e.player);
+                    tag.setString(Reference.MOD_ID + "doChatMessage", "noSend");
                 }
+            }else{
+                sendChatMessage(e.player);
+            }
+
+        }
+    }
+
+    private void sendChatMessage(EntityPlayer player){
+        if (ConfigurationHandler.doChatMessage) {
+            if (ConfigurationHandler.chatMessageColor == 1) {
+                player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.RED + ConfigurationHandler.chatMessage));
+            }else if (ConfigurationHandler.chatMessageColor == 2) {
+                player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.GREEN + ConfigurationHandler.chatMessage));
+            }else if (ConfigurationHandler.chatMessageColor == 3) {
+                player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.BLUE + ConfigurationHandler.chatMessage));
+            }else if (ConfigurationHandler.chatMessageColor == 4) {
+                player.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.GOLD + ConfigurationHandler.chatMessage));
+            }else{
+                player.addChatComponentMessage(new ChatComponentTranslation(ConfigurationHandler.chatMessage));
             }
         }
+    }
 
 }
--- src/main/java/com/miningmark48/miscconfigs/handler/ConfigurationHandler.java
+++ src/main/java/com/miningmark48/miscconfigs/handler/ConfigurationHandler.java
@@ -7,6 +7,7 @@ import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.config.Configuration;
 
 import java.io.File;
+import java.util.Locale;
 
 public class ConfigurationHandler {
 
@@ -14,6 +15,7 @@ public class ConfigurationHandler {
 
     public static boolean craftingTables;
     public static boolean doChatMessage;
+    public static boolean sendChatMessageOnce;
     public static String chatMessage;
     public static int chatMessageColor;
     public static int saplingGrowthMultiplier;
@@ -43,6 +45,7 @@ public class ConfigurationHandler {
 
         craftingTables = configuration.getBoolean(StatCollector.translateToLocal("config.craftingTables.title"), Configuration.CATEGORY_GENERAL, false, StatCollector.translateToLocal("config.craftingTables.desc"));
         doChatMessage = configuration.getBoolean(StatCollector.translateToLocal("config.doChatMessage.title"), StatCollector.translateToLocal("config.category.chatMessage.title"), true, StatCollector.translateToLocal("config.doChatMessage.desc"));
+        sendChatMessageOnce = configuration.getBoolean(StatCollector.translateToLocal("config.sendChatMessageOnce.title"), StatCollector.translateToLocal("config.category.chatMessage.title"), false, StatCollector.translateToLocal("config.sendChatMessageOnce.desc"));
         chatMessage = configuration.getString(StatCollector.translateToLocal("config.chatMessage.title"), StatCollector.translateToLocal("config.category.chatMessage.title"), StatCollector.translateToLocal("config.chatMessage.default"), StatCollector.translateToLocal("config.chatMessage.desc"));
         chatMessageColor = configuration.getInt(StatCollector.translateToLocal("config.chatMessageColor.title"), StatCollector.translateToLocal("config.category.chatMessage.title"), 4, 0, 4, StatCollector.translateToLocal("config.chatMessageColor.desc"));
         saplingGrowthMultiplier = configuration.getInt(StatCollector.translateToLocal("config.saplingGrowthMultiplier.title"), Configuration.CATEGORY_GENERAL, 0, 0, Integer.MAX_VALUE, StatCollector.translateToLocal("config.saplingGrowthMultiplier.desc"));
