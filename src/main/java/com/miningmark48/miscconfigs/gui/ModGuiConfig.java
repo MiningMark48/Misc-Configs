2dae9321bbedf99ed85ded8b6b2f6d9b1ba4a381
MiningMark48 <miningmark48@gmail.com>
2016-04-21 23:00:51 -0400

    FIxed mod config gui for message settings

--- src/main/java/com/miningmark48/miscconfigs/gui/ModGuiConfig.java
+++ src/main/java/com/miningmark48/miscconfigs/gui/ModGuiConfig.java
@@ -3,11 +3,13 @@ package com.miningmark48.miscconfigs.gui;
 import com.miningmark48.miscconfigs.handler.ConfigurationHandler;
 import com.miningmark48.miscconfigs.reference.Reference;
 import cpw.mods.fml.client.config.GuiConfig;
+import cpw.mods.fml.client.config.IConfigElement;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
+import scala.actors.threadpool.Arrays;
 
 import java.util.Locale;
 
@@ -15,7 +17,10 @@ public class ModGuiConfig extends GuiConfig{
 
     public ModGuiConfig(GuiScreen guiScreen){
         super(guiScreen,
-                new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                Arrays.asList(new IConfigElement[]{
+                    new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)),
+                    new ConfigElement(ConfigurationHandler.configuration.getCategory(StatCollector.translateToLocal("config.category.chatMessage.title").toLowerCase()))
+                }),
                 Reference.MOD_ID,
                 false,
                 false,
